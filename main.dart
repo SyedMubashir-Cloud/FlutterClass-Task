af3d468099350c7d6ffc7a4f47419a89e9ff70fa
SyedMubashir-Cloud <Muhammadmubashirmallick12@gmail.com>
2025-05-10 21:23:38 +0500

    Ques 4

--- main.dart
+++ main.dart
@@ -1,50 +1,15 @@
-//Ques1
-import 'dart:math';
+//Ques 4
+
 void main() {
-  int number = 12345; // Hardcoded number
-  int count = 0;
-  int tempNumber = number;
+  int number = 1;
+  int sum = 0;
 
-  // Special case: if the number is 0
-  if (tempNumber == 0) {
-    count = 1;
-  } else {
-    while (tempNumber > 0) {
-      tempNumber ~/= 10; // Integer division by 10
-      count++;
+  do {
+    if (number % 2 != 0) {
+      sum += number;
     }
-  }
+    number++;
+  } while (number <= 50);
 
-  print("The number of digits in $number is: $count");
-
-   //Ques2
-
-  int passwordLength = 12; // Hardcoded password length
-  const String chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#\$%^&*()';
-  
-  String password = '';
-  Random random = Random();
-  int i = 0;
-
-  while (i < passwordLength) {
-    int index = random.nextInt(chars.length);
-    password += chars[index];
-    i++;
-  }
-
-  print('Generated Password: $password');
-
-//Ques 3
-
-  String inputString = "Hello, Dart!"; // Hardcoded string
-
-  if (inputString.isEmpty) {
-    print("The string is empty.");
-  } else {
-    print("The string is not empty.");
-  }
-
-
-
-
+  print("The sum of odd numbers from 1 to 50 is: $sum");
 }
