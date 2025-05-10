d10bbd0fa65b18a507caff0e6cb072fed02d589b
SyedMubashir-Cloud <Muhammadmubashirmallick12@gmail.com>
2025-05-10 22:37:42 +0500

    Ques10

--- main.dart
+++ main.dart
@@ -118,6 +118,15 @@ void main() {
     i++;
   } while (i <= 20);
 }
+//Ques10
+{
+  int number = 5;
+
+  print("Multiplication Table of $number:");
+  for (int i = 1; i <= 10; i++) {
+    print("$number x $i = ${number * i}");
+  }
+}
 
 
 }
