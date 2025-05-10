d4cdbe319b23fc9d5e4ad7417d902f497fdf81eb
SyedMubashir-Cloud <Muhammadmubashirmallick12@gmail.com>
2025-05-10 22:30:11 +0500

    Ques8

--- main.dart
+++ main.dart
@@ -89,6 +89,24 @@ void main() {
     print(i);
     i--;
   }
+   {
+  List<int> numbers = [10, 20, 30, 40, 50];
+  int sum = 0;
+  int i = 0;
+
+  if (numbers.isNotEmpty) {
+    do {
+      sum += numbers[i];
+      i++;
+    } while (i < numbers.length);
+
+    double average = sum / numbers.length;
+    print("Average: $average");
+  } else {
+    print("The list is empty. Cannot compute average.");
+  }
+}
+
 }
 
 
